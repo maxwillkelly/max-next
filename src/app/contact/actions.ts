bd946a272fc307dd6b5d7969ba7201f86bde6003
Max Kelly <maxwill.kelly@gmail.com>
2023-09-22 21:53:31 +0100

    feat(main): import api key

--- src/app/contact/actions.ts
+++ src/app/contact/actions.ts
@@ -1,7 +1,10 @@
 "use server";
 
+import { env } from "@/env.mjs";
+
 import { Message } from "./page";
 
 export const sendMessage = async (data: Message) => {
   console.log(JSON.stringify(data, null, 2));
+  console.log('RESEND_API_KEY', env.RESEND_API_KEY);
 };
