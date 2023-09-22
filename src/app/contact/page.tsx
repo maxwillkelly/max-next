4b7e2d91d7ac196935a932f284cee1bc773432a7
Max Kelly <maxwill.kelly@gmail.com>
2023-09-22 21:09:48 +0100

    feat(main): Contact react hook form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,74 +1,111 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@nextui-org/button";
 import { Card, CardBody, CardHeader } from "@nextui-org/card";
 import { Input } from "@nextui-org/input";
 import { Textarea } from "@nextui-org/input";
-import { Link } from "@nextui-org/link";
 import { SendHorizontal } from "lucide-react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+
+export const messageSchema = z.object({
+  firstName: z.string().min(1, "First name is required"),
+  lastName: z.string().min(1, "Last name is required"),
+  email: z.string().email(),
+  subtitle: z.string().min(1, "Subtitle is required"),
+  message: z.string().min(1, "Message is required"),
+});
+
+export type Message = z.infer<typeof messageSchema>;
 
 const ContactPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Message>({
+    resolver: zodResolver(messageSchema),
+  });
+
+  const onSubmit: SubmitHandler<Message> = (data) => {
+    console.log(JSON.stringify(data, null, 2));
+  };
+
   return (
-    <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-16">
-      <div className="container flex min-w-full flex-col items-center gap-4 text-center">
-        <h1 className="text-2xl font-bold sm:text-4xl md:text-5xl lg:text-6xl">
-          Contact
-        </h1>
-        <p className="max-w-6xl sm:text-lg sm:leading-8 lg:px-48">
-          If you have any questions or would like to get in touch, please feel
-          free to send me an email at{" "}
-          <Link href="mailto:maxwill.kelly@gmail.com">
-            maxwill.kelly@gmail.com
-          </Link>{" "}
-          or use this form below to send me a message.
-        </p>
-        <Card className="w-11/12 max-w-lg sm:w-9/12 md:w-8/12">
-          <CardHeader>
-            <h2 className="font-semibold sm:text-lg sm:leading-8">
-              Contact me
-            </h2>
-          </CardHeader>
-          <CardBody>
-            <form className="flex flex-col flex-wrap">
-              <div className="mb-6 md:flex">
-                <Input
-                  type="text"
-                  label="First Name"
-                  placeholder="John"
-                  className="mb-6 px-3 md:mb-0 md:w-1/2"
-                />
-                <Input
-                  type="text"
-                  label="Last Name"
-                  placeholder="Smith"
-                  className="px-3 md:w-1/2"
-                />
-              </div>
-              <div className="mb-6 md:flex">
-                <Input
-                  type="email"
-                  label="Email"
-                  placeholder="john.smith@email.com"
-                  className="mb-6 px-3 md:mb-0 md:w-1/2"
-                />
-                <Input
-                  type="subtitle"
-                  label="Subtitle"
-                  placeholder="Hello"
-                  className="px-3 md:w-1/2"
-                />
-              </div>
-              <Textarea
-                label="Message"
-                placeholder="Let's rock 'n' roll"
-                className="mb-6 px-3"
-              />
-              <Button color="danger" endContent={<SendHorizontal />}>
-                Send message
-              </Button>
-            </form>
-          </CardBody>
-        </Card>
-      </div>
-    </section>
+    <Card className="w-11/12 max-w-lg sm:w-9/12 md:w-8/12">
+      <CardHeader>
+        <h2 className="font-semibold sm:text-lg sm:leading-8">Contact me</h2>
+      </CardHeader>
+      <CardBody>
+        <form
+          className="flex flex-col flex-wrap"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="mb-6 md:flex">
+            <Input
+              type="text"
+              label="First Name"
+              placeholder="John"
+              className="mb-6 px-3 md:mb-0 md:w-1/2"
+              isInvalid={Boolean(errors.firstName)}
+              errorMessage={
+                errors.firstName?.message || <div className="h-4"></div>
+              }
+              {...register("firstName")}
+            />
+            <Input
+              type="text"
+              label="Last Name"
+              placeholder="Smith"
+              className="px-3 md:w-1/2"
+              isInvalid={Boolean(errors.lastName)}
+              errorMessage={
+                errors.lastName?.message || <div className="h-4"></div>
+              }
+              {...register("lastName")}
+            />
+          </div>
+          <div className="mb-6 md:flex">
+            <Input
+              type="email"
+              label="Email"
+              placeholder="john.smith@email.com"
+              className="mb-6 px-3 md:mb-0 md:w-1/2"
+              isInvalid={Boolean(errors.email)}
+              errorMessage={
+                errors.email?.message || <div className="h-4"></div>
+              }
+              {...register("email")}
+            />
+            <Input
+              type="subtitle"
+              label="Subtitle"
+              placeholder="Hello"
+              className="px-3 md:w-1/2"
+              isInvalid={Boolean(errors.subtitle)}
+              errorMessage={
+                errors.subtitle?.message || <div className="h-4"></div>
+              }
+              {...register("subtitle")}
+            />
+          </div>
+          <Textarea
+            label="Message"
+            placeholder="Let's rock 'n' roll"
+            className="mb-6 px-3"
+            isInvalid={Boolean(errors.message)}
+            errorMessage={
+              errors.message?.message || <div className="h-4"></div>
+            }
+            {...register("message")}
+          />
+          <Button type="submit" color="danger" endContent={<SendHorizontal />}>
+            Send message
+          </Button>
+        </form>
+      </CardBody>
+    </Card>
   );
 };
 
