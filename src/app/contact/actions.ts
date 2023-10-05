2a55f02cc6e2e8c962e9a0faeda21c5ae79f4205
Max Kelly <maxwill.kelly@gmail.com>
2023-10-05 22:43:37 +0100

    fix(main): Parse schema

--- src/app/contact/actions.ts
+++ src/app/contact/actions.ts
@@ -1,21 +1,23 @@
 "use server";
 
-import { Resend } from 'resend';
+import { Message, messageSchema } from "./_components/ContactForm";
+import ContactEmailTemplate from "@/emails/ContactEmailTemplate";
 import { env } from "@/env.mjs";
-import { Message } from "./page";
-import ContactEmailTemplate from '@/emails/ContactEmailTemplate';
+import { Resend } from "resend";
 
 const resend = new Resend(env.RESEND_API_KEY);
 
-export const sendMessage = async (message: Message) => {  
+export const sendMessage = async (message: Message) => {
+  messageSchema.parse(message);
+
   const { firstName, lastName } = message;
 
   const data = resend.emails.send({
-    from: 'Max Next <onboarding@resend.dev>',
+    from: "Max Next <onboarding@resend.dev>",
     to: [env.SEND_EMAIL],
     subject: `You have received a message from ${firstName} ${lastName}`,
     react: ContactEmailTemplate({ message, nodeEnv: env.NODE_ENV }),
-  })
+  });
 
   return data;
 };
