5d1bd77539805f14ceb17c6831606f5c9f5a8f3d
Max Kelly <maxwill.kelly@gmail.com>
2023-10-11 19:04:07 +0100

    fix(main): Padding in project page

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,7 +1,12 @@
-import { runQuery } from "@/sanity/lib/fetch";
-import { q } from "groqd";
-import { groq } from "next-sanity";
 import { projectSelection } from "../types";
+import { runQuery } from "@/sanity/lib/fetch";
+import { Button } from "@nextui-org/button";
+import { Card, CardBody, CardHeader } from "@nextui-org/card";
+import { Divider } from "@nextui-org/divider";
+import { q } from "groqd";
+import { ArrowLeft } from "lucide-react";
+import { groq } from "next-sanity";
+import NextLink from "next/link";
 
 interface Props {
   params: {
@@ -18,21 +23,37 @@ const ProjectPage = async ({ params }: Props) => {
     ),
   );
 
-  const { title, body } = project
+  const { title, body } = project;
 
   return (
     <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-16">
-      <div className="container flex min-w-full flex-col items-center gap-4 text-center">
-        <h1 className="text-2xl font-bold sm:text-4xl md:text-5xl lg:text-6xl">
-          {title}
-        </h1>
-        <p className="max-w-6xl sm:text-lg sm:leading-8 lg:px-48">
-          {body}
-        </p>
-        <div className="flex flex-1 pt-2">
-          <div>{JSON.stringify(project, null, 2)}</div>
+      <div className="container flex min-w-full flex-col gap-4 px-6 sm:px-12 md:px-24 lg:px-48">
+        <div className="flex flex-col items-start">
+          <Button
+            as={NextLink}
+            href="/projects"
+            color="danger"
+            variant="light"
+            startContent={<ArrowLeft />}
+            className="p-0 data-[hover=true]:bg-transparent"
+          >
+            Projects
+          </Button>
+          <h1 className="text-xl font-bold sm:text-3xl md:text-4xl lg:text-5xl">
+            {title}
+          </h1>
+        </div>
+        <div>
+          <p className="max-w-6xl sm:text-lg sm:leading-8">{body}</p>
         </div>
       </div>
+      {/* <h1 className="text-2xl font-bold sm:text-4xl md:text-5xl lg:text-6xl">
+          {title}
+        </h1>
+        <p className="max-w-6xl sm:text-lg sm:leading-8 lg:px-48">{body}</p>
+        <div className="flex flex-1 pt-2">
+          <div>{JSON.stringify(project, null, 2)}</div>
+        </div> */}
     </section>
   );
 };
