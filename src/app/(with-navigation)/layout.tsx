666da3d0db4a89fd4528f8da7684e80c7c103d90
Max Kelly <maxwill.kelly@gmail.com>
2023-10-13 21:38:30 +0100

    fix(main): Adds tailwind weights

--- src/app/(with-navigation)/layout.tsx
+++ src/app/(with-navigation)/layout.tsx
@@ -7,6 +7,7 @@ import { Raleway } from "next/font/google";
 
 const raleway = Raleway({
   subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   fallback: [
     "ui-sans-serif",
     "system-ui",
