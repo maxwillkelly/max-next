4f624e8e7aa372c336831fa57e47210d6d8acb59
Max Kelly <maxwill.kelly@gmail.com>
2023-10-16 15:39:20 +0100

    fix(main): Refactoring

---
+++ src/app/(with-navigation)/experience/_components/ExperienceTabs.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import EducationItems from "./experience-tabs/EducationItems";
+import WorkItems from "./experience-tabs/WorkItems";
+import { Tabs, Tab } from "@nextui-org/tabs";
+import { Briefcase, GraduationCap } from "lucide-react";
+import { useSearchParams, useRouter } from "next/navigation";
+import { Key, useCallback } from "react";
+
+enum TabOptions {
+  WORK = "work",
+  EDUCATION = "education",
+}
+
+const ExperienceTabs = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const tab = searchParams.get("tab") as TabOptions;
+
+  // Get a new searchParams string by merging the current
+  // searchParams with a provided key/value pair
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams],
+  );
+
+  const handleTabChange = (value: Key) => {
+    router.push(
+      "/experience" + "?" + createQueryString("tab", value as string),
+    );
+  };
+
+  return (
+    <Tabs
+      aria-label="Experience type options"
+      color="danger"
+      size="lg"
+      selectedKey={tab}
+      onSelectionChange={handleTabChange}
+    >
+      <Tab
+        key="work"
+        title={
+          <div className="flex items-center space-x-2">
+            <Briefcase />
+            <span>Work</span>
+          </div>
+        }
+      >
+        <WorkItems />
+      </Tab>
+      <Tab
+        key="education"
+        title={
+          <div className="flex items-center space-x-2">
+            <GraduationCap />
+            <span>Education</span>
+          </div>
+        }
+      >
+        <EducationItems />
+      </Tab>
+    </Tabs>
+  );
+};
+
+export default ExperienceTabs;
--- src/app/(with-navigation)/experience/page.tsx
+++ src/app/(with-navigation)/experience/page.tsx
@@ -1,37 +1,6 @@
-"use client";
-
-import EducationItems from "./_components/EducationItems";
-import WorkItems from "./_components/WorkItems";
-import { Tabs, Tab } from "@nextui-org/tabs";
-import { Briefcase, GraduationCap } from "lucide-react";
-import { useSearchParams, useRouter } from "next/navigation";
-import { Key, useCallback } from "react";
-
-type TabOptions = "work" | "education";
+import ExperienceTabs from "./_components/ExperienceTabs";
 
 const ExperiencePage = () => {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const tab = searchParams.get("tab") as TabOptions;
-
-  // Get a new searchParams string by merging the current
-  // searchParams with a provided key/value pair
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams);
-      params.set(name, value);
-
-      return params.toString();
-    },
-    [searchParams],
-  );
-
-  const handleTabChange = (value: Key) => {
-    router.push(
-      "/experience" + "?" + createQueryString("tab", value as string),
-    );
-  };
-
   return (
     <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-16">
       <div className="container flex min-w-full flex-col items-center gap-4 text-center">
@@ -43,36 +12,7 @@ const ExperiencePage = () => {
           school and what I have been up to in my spare time.
         </p>
         <div className="pt-2">
-          <Tabs
-            aria-label="Experience type options"
-            color="danger"
-            size="lg"
-            selectedKey={tab}
-            onSelectionChange={handleTabChange}
-          >
-            <Tab
-              key="work"
-              title={
-                <div className="flex items-center space-x-2">
-                  <Briefcase />
-                  <span>Work</span>
-                </div>
-              }
-            >
-              <WorkItems />
-            </Tab>
-            <Tab
-              key="education"
-              title={
-                <div className="flex items-center space-x-2">
-                  <GraduationCap />
-                  <span>Education</span>
-                </div>
-              }
-            >
-              <EducationItems />
-            </Tab>
-          </Tabs>
+          <ExperienceTabs />
         </div>
       </div>
     </section>
